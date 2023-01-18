aec32b1f20d64ee583c7ae2865c8a42297cdbb26
huylinh <nguyenhuylinhvt98@gmail.com>
2023-01-18 20:17:37 +0700

    Add home

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -42,7 +42,7 @@ class BooksController < ApplicationController
         format.json { render :show, status: :ok, location: @book }
       else
         format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @book.errors, status: :unprocessable_entity }
+        format.json   { render json: @book.errors, status: :unprocessable_entity }
       end
     end
   end
